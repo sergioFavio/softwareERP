2a58d0fc1d22f886467d2303691488813da14388
Sergio Hernán Valenzuela Cámara <serginho61@gmail.com>
2017-05-29 12:01:59 -0400

    actualización pedidos y pagos de Zúñiga

--- application/controllers/tienda.php
+++ application/controllers/tienda.php
@@ -673,8 +673,8 @@ class Tienda extends CI_Controller {
 	    	$deposito = $this->numeroDocumento_model->getNumero($nombreTabla);
 			
 			$anhoSistema = date("Y");	//... anho del sistema
- 			$anhoSistema = substr($anhoSistema, 0, 4);	//... anho del sistema
-
+// 			$anhoSistema = substr($anhoSistema, 0, 4);	//... anho del sistema
+$anhoSistema ='2016'; 
  				 					
 			if(strlen($deposito)==4 ){
 				$secuenciaDeposito= 0;  // toma los caracteres ... secuencia.
@@ -1541,9 +1541,7 @@ class Tienda extends CI_Controller {
 			
 			if($local=="Z"){		//...cuando el local es Z:Zuñiga...
 				$anhoSistema = date("Y");	//... anho del sistema
-//				$anhoSistema = substr($anhoSistema, 0, 4);	//... anho del sistema
-$anhoSistema ='2016';
-
+				$anhoSistema = substr($anhoSistema, 0, 4);	//... anho del sistema
 				 				
 				if(strlen($pedido)==8 ){
 					$secuenciaPedido= substr($pedido, 0,4);  // toma los caracteres ... secuencia.
